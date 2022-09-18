4244a3020bfd89317d003b7b4f7a0fb126671136
andrestuarr <44060427+andrestuarr@users.noreply.github.com>
2022-09-17 23:02:46 -0500

    add copy file to snowflake table

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import snowflake.connector
 def main():
     conn = snowflake.connector.connect(
         user='andrestuarr10000',
-        password='qwer.1234O',
+        password='XXXX',
         account='HD55087.us-central1.gcp',
         session_parameters={
-            'QUERY_TAG': 'EndOfMonthFinancials',
+            'QUERY_TAG': 'EndOfMonthFinancials'
         }
     )
-    conn.cursor().execute("CREATE WAREHOUSE IF NOT EXISTS rappibank_warehouse")
-    conn.cursor().execute("CREATE DATABASE IF NOT EXISTS rappibankdb")
-    conn.cursor().execute("CREATE SCHEMA IF NOT EXISTS andrestuarrdb")
-    conn.cursor().execute("PUT file:///Users/andres/Downloads/archive/members.csv* @%members")
-    conn.cursor().execute("COPY INTO members")
+    conn.cursor().execute('USE WAREHOUSE "rappibank"')
+    conn.cursor().execute('USE DATABASE "challenge"')
+    conn.cursor().execute('USE SCHEMA "andrestuarr"')
+    print(conn)
+    conn.cursor().execute('PUT file:///Users/andres/Downloads/archive/members.csv* @%"members" PARALLEL = 90')
+    print('termino')
+    conn.cursor().execute('''COPY INTO "members" FILE_FORMAT = (SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"' REPLACE_INVALID_CHARACTERS = TRUE)''')
 
     conn.close()
 
